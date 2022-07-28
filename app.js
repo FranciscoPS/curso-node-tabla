ea3d27c80001ac0539cef32cbfa46ce347dd44fd
@francisco.peralta1 <francisco.peralta@umvel.com>
2022-07-28 18:37:03 -0500

    34. Yargs

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const { crearArchivo } = require("./helpers/multiplicar");
+const argv = require("yargs").argv;
 
-// const base = 4;
+console.clear();
+
+//Podemos desestructurar el arrego, asi mismo podemos asignar un valor por defecto en caso de no recibir la variable o de que llegue en undefined
+// const [, , arg3 = "base=5"] = process.argv;
+
+//Podemos separar cuando se encuentre un =
+//const [, base = 5] = arg3.split("=");
+// console.log(base);
 
 // crearArchivo(base)
 //   .then((nombreArchivo) => console.log(nombreArchivo, "creado"))
 //   .catch((err) => console.log(err));
 
-console.clear();
-
 console.log(process.argv);
+console.log(argv);
 
-//Podemos desestructurar el arrego, asi mismo podemos asignar un valor por defecto en caso de no recibir la variable o de que llegue en undefined
-// const [, , arg3 = "base=5"] = process.argv;
-// console.log(arg3);
-
-//Podemos separar cuando se encuentre un =
-// const [, base = 5] = arg3.split("=");
-// console.log(base);
+const { base, limite } = argv;
+console.log(limite, base)
