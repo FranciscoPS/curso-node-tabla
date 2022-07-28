bd7e32701db86a1bbefff9fed4e13afad06ac626
@francisco.peralta1 <francisco.peralta@umvel.com>
2022-07-28 18:28:22 -0500

    33. package.json

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const { crearArchivo } = require("./helpers/multiplicar");
 
-const base = 4;
+// const base = 4;
 
-crearArchivo(base)
-  .then((nombreArchivo) => console.log(nombreArchivo, "creado"))
-  .catch((err) => console.log(err));
-;
+// crearArchivo(base)
+//   .then((nombreArchivo) => console.log(nombreArchivo, "creado"))
+//   .catch((err) => console.log(err));
+
+console.clear();
+
+console.log(process.argv);
+
+//Podemos desestructurar el arrego, asi mismo podemos asignar un valor por defecto en caso de no recibir la variable o de que llegue en undefined
+// const [, , arg3 = "base=5"] = process.argv;
+// console.log(arg3);
+
+//Podemos separar cuando se encuentre un =
+// const [, base = 5] = arg3.split("=");
+// console.log(base);
